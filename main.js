a23d2b725ba5d568274a4121619eedb8f02ba96d
Arno luyckx <A.luyckx@protonmail.com>
2023-02-15 13:45:33 +0100

    Added CDN's, Made comments in the main files for structure

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Handy extension! Works like this to color code..
+ * TODO (orange)
+ * ? (blue) Question / Help me out partner.
+ * ! (red) Warning / We should not forget this.
+ * regular green for miscellaneous comments.
+ * 
+ * Extension : Name: Better Comments
+    Id: aaron-bond.better-comments
+    Description: Improve your code commenting by annotating with alert, informational, TODOs, and more!
+    Version: 3.0.2
+    Publisher: Aaron Bond
+    VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+ */
