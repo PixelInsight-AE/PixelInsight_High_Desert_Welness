43b55c016cfa9d7a3f5dd58659f125ec6c8e8c06
Arno luyckx <A.luyckx@protonmail.com>
2023-02-21 19:26:21 +0100

    Interview questions

--- main.js
+++ main.js
@@ -12,3 +12,83 @@
     Publisher: Aaron Bond
     VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
  */
+
+function log() {
+  console.log('hello world')
+}
+
+log();
+
+
+"What is JavaScript and what are its key features?"
+    /*   Answer: JavaScript is a scripting language that is widely used for creating interactive web pages. Some of its key features include dynamic typing, prototype-based object-oriented programming, and the ability to manipulate the Document Object Model (DOM). */
+
+
+
+"How does JavaScript differ from other programming languages?"
+    /*   Answer: JavaScript is a dynamically typed language, meaning that data types can change during runtime. It is also an interpreted language, rather than a compiled language like C++. Additionally, JavaScript is primarily used for front-end development, whereas languages like Java and Python can be used for both front-end and back-end development. */
+
+
+
+"What is a closure in JavaScript and how is it used?"
+    /* Answer: A closure is a function that has access to variables in its outer function, even after that function has returned. Closures are often used to create private variables or to create functions that can be used in multiple contexts.
+    */
+
+
+
+"How do you handle errors in JavaScript?"
+    /* Answer: In JavaScript, errors can be handled using try-catch statements. The try block contains the code that may generate an error, while the catch block handles the error by providing an alternative code block to execute in case of an error. */
+
+
+
+"What is the difference between let and var in JavaScript?"
+    /* Answer: The let keyword was introduced in ES6 and is used to declare variables that have block scope, meaning they are only accessible within the block they are defined in. The var keyword, on the other hand, has function scope, meaning it is accessible within the entire function in which it is defined. */
+
+
+
+"Can you explain the concept of event bubbling in JavaScript?"
+    /* Answer: Event bubbling is a concept in JavaScript where an event triggered on a child element will also trigger on all of its parent elements. This can be problematic if you want to capture events only on the child element, but it can also be useful for delegating event handling to parent elements. */
+
+
+
+"What is the difference between == and === in JavaScript?"
+    /* Answer: The == operator is used for loose comparison, meaning it compares values without checking data types. The === operator is used for strict comparison, meaning it compares both values and data types. */
+
+
+
+"How do you work with asynchronous code in JavaScript?"
+    /* Answer: Asynchronous code in JavaScript is typically handled using callbacks, promises, or async/await syntax. Callbacks are functions that are called after a certain event has occurred, while promises and async/await are used to handle asynchronous operations that may or may not resolve successfully. */
+
+
+
+"Can you explain the difference between null and undefined in JavaScript?"
+    /* Answer: In JavaScript, null is a value that represents intentional absence of any object value, while undefined is a type that is automatically assigned to variables that have been declared but not yet initialized. */
+
+
+
+"How do you optimize JavaScript performance on a web page?"
+    /* Answer: Some ways to optimize JavaScript performance on a web page include minimizing the number of HTTP requests, compressing files, using asynchronous loading for scripts, and reducing the amount of global variables used in scripts. Additionally, using modern JavaScript features like arrow functions and the spread operator can improve performance compared to older syntax. */
+
+
+
+
+
+
+
+"Algorithm: Write a function that takes an array of integers and returns the sum of all the even numbers in the array."
+
+Solution:
+
+javascript
+function sumOfEvens(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+/*
+Explanation: The function sumOfEvens takes an array of integers as its input parameter. It then initializes a variable called sum to 0, which will store the sum of all the even numbers in the array. It then loops through each element in the array using a for loop, and checks if the current element is even using the modulus operator. If the current element is even, its value is added to the sum variable. Finally, the function returns the sum of all the even numbers in the array.*/
